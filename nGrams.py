8c5a34eebb7b2dacecf762534f73446e1b90fb8a
Mati <mati.halfa.92@gmail.com>
2020-06-01 21:36:09 +0300

    Update nGrams.py
    update n grams to work with lists

--- nGrams.py
+++ nGrams.py
@@ -1,27 +1,42 @@
+"""
+N grams - divide text file to parts 
 
-
-def myNgrams(file_name):
-   file_hiv = open(file_name, "r")
-   ngrams_size = 4
-   ngrams = {}
+    input: text file name
+           number of chars to read from file
+        
+    output: list of different parts  
     
-   #Start with the values that we need
-   unuse_line= file_hiv.readline()
-
+"""
+def nGrams(file_name,ngrams_size):
+   
+   file_reader=without_line_breaks(file_name)
+   
+   ngrams = []
+   
+   i=0
    while True:
-     getFromFile = file_hiv.read(ngrams_size)
-     
-     if getFromFile not in ngrams.keys():
-         ngrams[getFromFile] = []
-     ngrams[getFromFile].append(file_hiv.tell())
+     getFromFile = file_reader[i:i+ngrams_size]
+     i+=ngrams_size
      if len(getFromFile) < ngrams_size:
          break
-   file_hiv.close()
-   return ngrams
-    
+     if getFromFile not in ngrams: #without duplic..?
+      ngrams.append(getFromFile)
+        
+   return ngrams   
+def without_line_breaks(file_name):
+   
+ a_file = open(file_name, "r")
+ #Start with the values that we need
+ a_file.readline()
+ 
+ string_without_line_breaks = ""
+ for line in a_file:
+    stripped_line = line.rstrip()
+    string_without_line_breaks += stripped_line
+        
+ a_file.close()
+ return string_without_line_breaks
 
-ngrams_hiv = myNgrams("hiv.fasta")
-ngrams_sc = myNgrams("sc.fasta")
-
-
+# ngrams_hiv = nGrams("hiv.fasta",10)
+# ngrams_sc = nGrams("sc.fasta",10)
 

9fb5770169ff5b539997ef845b1154a4084bb5a2
Mati <mati.halfa.92@gmail.com>
2020-06-09 19:42:01 +0300

    update
    n grams + hist
    zv + R func

--- nGrams.py
+++ nGrams.py
@@ -7,33 +7,9 @@ N grams - divide text file to parts
     output: list of different parts  
     
 """
-import nltk
 from nltk import ngrams
 import matplotlib.pyplot as plt
-import numpy as np
 
-def nGrams(file_name,ngrams_size):
-   
-   file_reader=without_line_breaks(file_name)
-   
-   ngrams = []
-   freq = []
-   
-   i=0
-   while True:
-     getFromFile = file_reader[i:i+ngrams_size]
-     i+=ngrams_size
-     if len(getFromFile) < ngrams_size:
-         break
-     if getFromFile not in ngrams: #without duplic..?
-      ngrams.append(getFromFile)
-      freq.append(1)
-    
-     else:
-         freq[ngrams.index(getFromFile)]=freq[ngrams.index(getFromFile)]+1
-     
-        
-   return [ngrams,freq]   
 def without_line_breaks(file_name):
    
  a_file = open(file_name, "r")
@@ -47,10 +23,7 @@ def without_line_breaks(file_name):
         
  a_file.close()
  return string_without_line_breaks
-
-#matih
-# move one letter at the time
-def newNGrams(file_name,ngrams_size):
+def nGrams(file_name,ngrams_size):
     file_reader=without_line_breaks(file_name)
     
     list_hist =[]
@@ -69,33 +42,17 @@ def newNGrams(file_name,ngrams_size):
       freq[ngrams_array.index(s1)]=freq[ngrams_array.index(s1)]+1
      list_hist.append(s1)
     
+    #show Histogram
+     
+    plt.hist(list_hist,bins=len(freq))
+    plt.grid(axis='y', alpha=0.75)
+    plt.xlabel('N GRAMS')
+    plt.ylabel('Frequency')
+    plt.title('Histogram n grams= '+str(ngrams_size))
+    plt.show()
+    
     return [ngrams_array,freq,list_hist] 
 
-f = []
-ng = [1,2,3,4,5,6,7,8,9,10]
-for i in range(0,10):
-    [ngrams_hiv,f1] = nGrams("hiv.fasta",ng[i])
-    f.append(f1)
-# [ngrams_hiv,f1] = nGrams("hiv.fasta",ng)
-# [ngrams_sc,f2] = nGrams("sc.fasta",ng)
-
-# plt.hist( f1,ngrams_hiv)
-# plt.show()
-
-ngNew=4
-x = newNGrams("hiv.fasta",ngNew)
-
-
-plt.hist(x[2],bins=len(x[1]))
-# [n,bins,patches]=plt.hist(x[1], bins=arrayrange,histtype='stepfilled', color=['b'],alpha=0.7, rwidth=0.85)
-plt.grid(axis='y', alpha=0.75)
-plt.xlabel('N GRAMS')
-plt.ylabel('Frequency')
-plt.title('Mati Histogram n grams= '+str(ngNew))
-# plt.text(23, 45, r'$\mu=15, b=3$')
-plt.show()
-# maxfreq = n.max()
-# Set a clean upper y-axis limit.
 
 
 
--- zv.py
+++ zv.py
@@ -6,5 +6,12 @@ def zv(Di,deltaT):
         sigma+=R(Di,Dj)
     return (1/ T)*sigma
 
-        
-
+def R(Di,Dj):
+    d=[]
+    m=len(Di)
+    for jj in range(m):
+        d=Di[jj]-Dj[jj]
+    
+    d2=[dvalue*dvalue for dvalue in d]
+    d2sum=sum(d2,0)
+    return (1-(6*d2sum)/(m*(m**2-1)));

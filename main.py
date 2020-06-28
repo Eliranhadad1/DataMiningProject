671f1418641dab00bd4a540c68ec586d574ffc9d
Mati <mati.halfa.92@gmail.com>
2020-06-28 17:04:59 +0300

    finish All

--- main.py
+++ main.py
@@ -8,27 +8,72 @@ Created on Fri Jun  5 08:20:58 2020
 
 import scipy.stats as ss
 import numpy as np
-from zv import zv
 import matplotlib.pyplot as plt
 from mykmedoids import KMedoids
-from nltk import ngrams
+from nltk import ngrams 
 import nltk
+from fuzzywuzzy import fuzz
+import operator
+from scipy.stats import spearmanr
 
-def calculateNGrams(D,minN,maxN):
+def zv(Di,deltaT):
+    
+    T=len(deltaT)
+    if T==0:
+        return 
+    
+    sigma=0;
+    for Dj in deltaT:
+         di=Di
+         toAdd = len(di)-len(Dj)
+         if toAdd>0:
+          maxDj=max(Dj)
+          for jj in range(toAdd):
+              Dj.append(maxDj+jj+1)
+              
+         elif toAdd<0:
+          maxDi=max(di)
+          for jj in range(abs(toAdd)):
+              di.append(maxDi+jj+1)
+              
+         [c,p]=spearmanr(di,Dj)
+         sigma+=c
+    return (1/ T)*sigma
+
+def calculateNGrams(D,n,tolerancePrecent):
+    
    
    Dngrams=[]
+   
    for j in range(len(D)):
-     Dngrams.append([])
-     for i in range(minN,maxN+1):
-         string_tuples=ngrams(D[j],i)
-         fdist = nltk.FreqDist(string_tuples)
-         f=0
-         for [k,z] in fdist.items():
-              f+=1
-
-         Dngrams[j].append(f)
+    Dngrams.append(0)
+    Dngrams[j]={}
+    string_tuples=ngrams(D[j],n)
+    fdist = nltk.FreqDist(string_tuples)
     
-   return Dngrams   
+    for [key,freq] in fdist.items():
+        # key = ''.join(key)
+        # if key=="CCAACT":
+        #     print('x')
+        fuzzyList={}
+        for k in Dngrams[j].keys():
+            fuzr = fuzz.ratio(key,k)
+            if  fuzr >= tolerancePrecent:
+                fuzzyList[k]=fuzr
+
+        if  len(fuzzyList)== 0:
+            Dngrams[j][key]=freq
+        else:
+            maxKey=max(fuzzyList.items(), key=operator.itemgetter(1))[0]
+            Dngrams[j][maxKey]=Dngrams[j][maxKey]+freq
+    
+                
+   DfrenkValues=[]
+   for d in Dngrams:
+        DfrenkValues.append(list(d.values()))
+    
+   return Dngrams,DfrenkValues
+
 def calculateRank(Di):
     # rankArray=len(Di)-ss.rankdata(Di,method='min').astype(int)
     rankArray=ss.rankdata(Di,method='min').astype(int)
@@ -53,16 +98,16 @@ def getRankByD(D):
       ranks.append(calculateRank(D[j]))
       
     return ranks
-def calculateZv(rank_file):
+def calculateZv(D):
     pi=[]
-    deltaTi=doAppend(len(rank_file))
+    deltaTi=doAppend(len(D))
    
-    for d in range(0,len(rank_file)):
-        deltaTi[d]=rank_file[0:d]
+    for d in range(len(D)):
+        deltaTi[d]=D[0:d]
     
    
-    for r in range(len(rank_file)):
-        pi.append(zv(rank_file[r],deltaTi[r]))
+    for r in range(len(D)):
+        pi.append(zv(D[r],deltaTi[r]))
   
     
     return [pi,deltaTi]
@@ -148,75 +193,113 @@ def ShowHist(x,y):
     
     # x = ranks
     listForHist = []
+    listForHist=y
+    # for ng in range(len(x)):
+    #  for r in range(y[ng]):
+    #         listForHist.append(ng+3)
     
-    for ng in range(len(x)):
-     for r in range(y[ng]):
-            listForHist.append(ng+3)
+    # x = np.arange(len(y))
+    plt.bar(x, height=y)
+    # plt.xticks(x, ['a','b','c'])
     
-    plt.hist(listForHist, bins=8, color='#0504aa',alpha=0.7, rwidth=0.7)
-    plt.grid(axis='y', alpha=0.75)
-    plt.xlabel('N GRAMS')
-    plt.ylabel('Ranks')
+    # plt.hist(listForHist, bins=len(y), density=True, color='#0504aa',alpha=0.7, rwidth=0.2)
+    # plt.grid(axis='y', alpha=0.75)
+    plt.xlabel('N GRAMS Keys')
+    plt.ylabel('Freq')
     plt.title('Histogram D')
     plt.show()
+def makeClusters(X,k):
+    kmedoids = KMedoids(n_clusters=k, random_state=0).fit(X)
+    l=kmedoids.labels_
+
+    d_index=[]
+    for jj in range(len(X)):
+        d_index.append(jj)    
+    showGraph(d_index, l, 'Di', 'cluster', 'KMedoids K=2')
+    return l
 ####################################
-
-
-#Measurement of file 1
-fileName="sc.fasta"
-m=200
-D = preProcessing(fileName, m)
-
-# #n grams from 4-10
-ngrams_D=calculateNGrams(D,3,10)
-rank_file1 = getRankByD(ngrams_D)
-for di in range(len(rank_file1)):
-    ShowHist(ngrams_D[di],rank_file1[di])
-
-[zv_file1,Delta_i]=calculateZv(rank_file1)
-
-showZVGraphs(D,zv_file1,fileName)
-
-#Measurement of file 2
-fileName="hiv.fasta"
-
-D = preProcessing(fileName, m)
-
-# #n grams from 4-10
-ngrams_D=calculateNGrams(D,3,10)
-rank_file2 = getRankByD(ngrams_D)
-
-for di in range(len(rank_file1)):
-    ShowHist(ngrams_D[di],rank_file2[di])
-
-[zv_file2,Delta_j]=calculateZv(rank_file2)
-
-showZVGraphs(D,zv_file2,fileName)
-
-
-#Measurement of DZV
-dzv_matrix=calculateAllDZV(rank_file1,rank_file2,Delta_i,Delta_j,zv_file1,zv_file2)
-showDZVGraph(dzv_matrix)
-
-# Dividing DZV matrix
-# until di in the rows
-firstDZV = dzv_matrix[1:len(rank_file1)]
-secondDZV = dzv_matrix[len(rank_file1)+1:]
-
-y=dzv_matrix
-y=np.delete(y,0,0)
-y=np.delete(y,m-1,0)
-y=np.delete(y,0,1)
-y=np.delete(y,m-1,1)
-
-X = y
-
-kmedoids = KMedoids(n_clusters=2, random_state=0).fit(X)
-l=kmedoids.labels_
-c=kmedoids.cluster_centers_
-d_index=[]
-for jj in range(len(y)):
-    d_index.append(jj)    
-showGraph(d_index, l, 'Di', 'cluster', 'KMedoids K=2')
+def calculateTheFilesData(n):
+    
+    #Measurement of file 1
+    fileName="sc.fasta"
+    m=100
+    D = preProcessing(fileName, m)
+    
+    # #n grams from 4-10
+    ngrams_D,DfrenqValues1=calculateNGrams(D,n,85)
+    
+    # for di in range(m):
+    #     ShowHist(range(len(ngrams_D[di].keys())),list(ngrams_D[di].values()))
+    
+    
+    [zv_file1,Delta_i]=calculateZv(DfrenqValues1)
+    
+    showZVGraphs(D,zv_file1,fileName)
+    
+    #Measurement of file 2
+    fileName="hiv.fasta"
+    
+    D = preProcessing(fileName, m)
+    
+    # #n grams from 4-10
+    ngrams_D,DfrenqValues2=calculateNGrams(D,n,85)
+    
+    for di in range(m):
+        ShowHist(range(len(ngrams_D[di].keys())),list(ngrams_D[di].values()))
+    
+    
+    [zv_file2,Delta_j]=calculateZv(DfrenqValues2)
+    
+    showZVGraphs(D,zv_file2,fileName)
+    
+    
+    #Measurement of DZV
+    dzv_matrix=calculateAllDZV(DfrenqValues1,DfrenqValues2,Delta_i,Delta_j,zv_file1,zv_file2)
+    showDZVGraph(dzv_matrix)
+    
+    # Dividing DZV matrix
+    # until di in the rows
+    firstDZV = dzv_matrix[1:len(DfrenqValues1)]
+    secondDZV = dzv_matrix[len(DfrenqValues2)+1:]
+    
+    y=dzv_matrix
+    y=np.delete(y,0,0)
+    y=np.delete(y,m-1,0)
+    y=np.delete(y,0,1)
+    y=np.delete(y,m-1,1)
+    
+    l=makeClusters(y,2)
+    
+    label_1 = 1
+    label_2 = 0
+    
+    errorCount=0
+    flag=0
+    for hh in range(len(l)):
+        if errorCount>=4:
+            flag=1
+        if hh < m/2-1:
+          if l[hh]==label_2:
+            errorCount+=1
+          else:
+              errorCount=0
+        else:
+            if l[hh]==label_2:
+                 errorCount+=1
+            else:
+              errorCount=0
+    if flag==0:
+        print("Corona is not transgenic!")
+    else:
+        print("Corona is transgenic!")
+    
+    retMatrix=[]
+    for row in firstDZV:
+        retMatrix.append(row[:100])
+    
+    return retMatrix 
 ##########################
 
+onlyCoronaDZV = calculateTheFilesData()
+
+makeClusters(onlyCoronaDZV,2)
